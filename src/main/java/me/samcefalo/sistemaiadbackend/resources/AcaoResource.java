a6ada5e2e7277925112ab7a6b1c34eb465a01fe3
SamCefalo <samuel.cefalo@gmail.com>
2022-03-29 00:21:44 -0300

    Adicionado repositorio para acao

---
+++ src/main/java/me/samcefalo/sistemaiadbackend/resources/AcaoResource.java
@@ -0,0 +1,56 @@
+package me.samcefalo.sistemaiadbackend.resources;
+
+import me.samcefalo.sistemaiadbackend.domain.Acao;
+import me.samcefalo.sistemaiadbackend.domain.dto.AcaoDTO;
+import me.samcefalo.sistemaiadbackend.services.AcaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/acoes")
+public class AcaoResource {
+
+    @Autowired
+    private AcaoService acaoService;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<AcaoDTO> find(@PathVariable int id) {
+        Acao acao = acaoService.find(id);
+        return ResponseEntity.ok().body(acaoService.getAcaoDto(acao));
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<AcaoDTO>> findAll() {
+        return ResponseEntity.ok()
+                .body(acaoService.getListAcaoDto(acaoService.findAll()));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody AcaoDTO acaoDTO) {
+        Acao acao = acaoService.getAcao(acaoDTO);
+        acao = acaoService.insert(acao);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(acao.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody AcaoDTO acaoDTO, @PathVariable int id) {
+        Acao acao = acaoService.getAcao(acaoDTO);
+        acao.setId(id);
+        acaoService.update(acao);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable int id) {
+        acaoService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
