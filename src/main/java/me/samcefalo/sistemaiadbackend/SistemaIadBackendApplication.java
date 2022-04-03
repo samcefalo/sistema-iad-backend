c2d3b24f97fd436044e9b786f6966da44c7c4485
SamCefalo <samuel.cefalo@gmail.com>
2022-04-03 15:09:46 -0300

    Criado testes para Put

--- src/main/java/me/samcefalo/sistemaiadbackend/SistemaIadBackendApplication.java
+++ src/main/java/me/samcefalo/sistemaiadbackend/SistemaIadBackendApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SistemaIadBackendApplication {
     
-    //TODO Pensar na possibilidade de criar um OUTPOST e INPUT DTO ou manter o mesmo DTO
     //TODO adicionar Placar para jogos
     //TODO adicionar pesquisa para resources
     public static void main(String[] args) {
