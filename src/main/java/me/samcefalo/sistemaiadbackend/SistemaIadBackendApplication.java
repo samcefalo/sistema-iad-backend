6fb7142b238432048dc1eb9603e2981fe37de63f
SamCefalo <samuel.cefalo@gmail.com>
2022-04-30 16:55:46 -0300

    Adicionado Bucket4j para limitar requisições

--- src/main/java/me/samcefalo/sistemaiadbackend/SistemaIadBackendApplication.java
+++ src/main/java/me/samcefalo/sistemaiadbackend/SistemaIadBackendApplication.java
@@ -2,8 +2,10 @@ package me.samcefalo.sistemaiadbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SistemaIadBackendApplication {
 
 
