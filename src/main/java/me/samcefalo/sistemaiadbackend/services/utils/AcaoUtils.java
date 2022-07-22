29f320d4a06a70b2209523be5a68b0064019be06
SamCefalo <samuel.cefalo@gmail.com>
2022-07-22 16:01:34 -0300

    Refatorado código de Criterias e Specifications

--- src/main/java/me/samcefalo/sistemaiadbackend/services/utils/AcaoUtils.java
+++ src/main/java/me/samcefalo/sistemaiadbackend/services/utils/AcaoUtils.java
@@ -1,8 +1,6 @@
 package me.samcefalo.sistemaiadbackend.services.utils;
 
 import me.samcefalo.sistemaiadbackend.models.Acao;
-import me.samcefalo.sistemaiadbackend.services.exceptions.ObjectNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,19 +9,6 @@ import java.util.stream.Stream;
 
 @Component
 public class AcaoUtils {
-
-    @Autowired
-    private ClassUtils classUtil;
-
-    public Class<?> getClass(String categoria) {
-        Class<?> categoriaClass;
-        try {
-            categoriaClass = classUtil.getAcaoClass(categoria);
-        } catch (ClassNotFoundException e) {
-            throw new ObjectNotFoundException("Acao não encontrada. ID: " + categoria + ", Tipo: " + Acao.class.getSimpleName());
-        }
-        return categoriaClass;
-    }
 
     public List<Acao> getOnlyExito(List<Acao> acoes) {
         Stream<Acao> acaoStream = acoes.stream();
